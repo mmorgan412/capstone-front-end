644217b51421e14a0aa94affb48a30768a485993
Melanie Gelinas <melanie.morgan@libertymutual.com>
2017-11-20 19:20:32 -0500

    Add authentication code
    Note - doesn't yet work

---
+++ assets/scripts/auth/authevents.js
@@ -0,0 +1,74 @@
+'use strict'
+
+const getFormFields = require(`../../../lib/get-form-fields`)
+const authapi = require('./authapi')
+const authui = require('./authui')
+
+const onSignInFormShow = function (event) {
+  event.preventDefault()
+  $('#sign-up-form').modal('hide')
+}
+
+const onSignUpFormShow = function (event) {
+  console.log('hittng onSignUpFormShow')
+  event.preventDefault()
+  $('#sign-in-form').modal('hide')
+}
+
+const onSignUp = function (event) {
+  console.log('hitting onSignUp authevents.js')
+  event.preventDefault()
+  const data = getFormFields(this)
+  authapi.signUp(data)
+    .then(authui.signUpSuccess)
+    .then(authapi.signUpSignIn)
+    .then(authui.signInSuccess)
+    .catch(authui.signUpFailure)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  const data = getFormFields(this)
+  authapi.signIn(data)
+    .then(authui.signInSuccess)
+    .catch(authui.signInFailure)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  const data = getFormFields(this)
+  authapi.changePassword(data)
+    .then(authui.changePasswordSuccess)
+    .catch(authui.changePasswordFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  authapi.signOut()
+    .then(authui.signOutSuccess)
+    .catch(authui.signOutFailure)
+}
+
+const addAuthHandlers = function () {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onChangePassword)
+  $('#sign-out-link').on('click', onSignOut)
+  $('#sign-in-form').on('focus', onSignInFormShow)
+  $('#sign-up-form').on('focus', onSignUpFormShow)
+  $('#sign-up-form').trigger('reset')
+  authui.initializeForm()
+  $('.close-modal').on('click', function () {
+    $('#change-password').trigger('reset')
+  })
+  $('.close-modal').on('click', function () {
+    $('#sign-up').trigger('reset')
+  })
+  $('.close-modal').on('click', function () {
+    $('#sign-in').trigger('reset')
+  })
+}
+
+module.exports = {
+  addAuthHandlers
+}
---
+++ assets/scripts/auth/authui.js
@@ -0,0 +1,124 @@
+'use strict'
+
+const store = require('../store')
+
+const alertCallerAuthSuccess = (alertLocation, alertMessage) => {
+  $('#' + alertLocation).addClass('alert alert-success').html(alertMessage)
+  setTimeout(function () {
+    $('#' + alertLocation).removeClass('alert alert-success').html('')
+  }, 1500)
+}
+
+const alertCallerAuthFailure = (alertLocation, alertMessage) => {
+  $('#' + alertLocation).addClass('alert alert-danger').html(alertMessage)
+  setTimeout(function () {
+    $('#' + alertLocation).removeClass('alert alert-danger').html('')
+  }, 1500)
+}
+
+const failureAuthShake = (modalTarget) => {
+  $('#' + modalTarget).addClass('shake')
+  setTimeout(function () {
+    $('#' + modalTarget).removeClass('shake')
+  }, 1500)
+}
+
+const signUpSuccess = (data) => {
+  console.log('hitting sign up success')
+  $('#sign-up-form').modal('hide')
+  // Used to clear out login data
+  $('#sign-up-form').on('hidden.bs.modal', function () {
+    $(this).find('form').trigger('reset')
+  })
+  alertCallerAuthSuccess('frontSuccess', 'Sign-Up Success')
+}
+
+const signUpFailure = () => {
+  console.log('hitting sign up failure')
+  alertCallerAuthFailure('frontError', 'Sign-Up Failure')
+  failureAuthShake('sign-up-form')
+}
+
+const signInSuccess = (response) => {
+  $('#sign-in-message').text('Sign In to Load Files')
+  $('#sign-in-email').removeData()
+  $('#sign-in-password').val('')
+  $('#sign-in-form').modal('hide')
+  $('#sign-in-link').hide()
+  $('#sign-up-link').hide()
+  $('#sign-out-link').show()
+  $('#change-password-link').show()
+  $('#create-ads-link').show()
+  $('#manage-ads-link').show()
+  $('#uploads-thumbnails').show()
+  $('#get-uploads-link').hide()
+  $('#get-user-uploads-link').show()
+  $('#file-upload-link').hide()
+  $('#file-upload-all-link').show()
+  // Used to clear out login data
+  $('#sign-in-form').on('hidden.bs.modal', function () {
+    $(this).find('form').trigger('reset')
+  })
+  store.user = response.user
+  $('#get-uploads-link').trigger('click')
+  alertCallerAuthSuccess('frontSuccess', 'Sign-In Success')
+}
+
+const signInFailure = () => {
+  alertCallerAuthFailure('frontError', 'Sign-In Failure')
+  failureAuthShake('sign-in-form')
+}
+
+const changePasswordSuccess = () => {
+  $('#change-password-form').modal('hide')
+  // Used to clear out login data
+  $('#change-password-form').on('hidden.bs.modal', function () {
+    $(this).find('form').trigger('reset')
+  })
+  alertCallerAuthSuccess('frontSuccess', 'Change Password Success')
+}
+
+const changePasswordFailure = () => {
+  alertCallerAuthFailure('frontError', 'Change Password Failure')
+  failureAuthShake('change-password-form')
+}
+
+const initializeForm = () => {
+  $('#sign-out').hide()
+  $('#sign-out-link').hide()
+  $('#change-password-link').hide()
+  $('#file-upload-link').hide()
+  $('#files-link').hide()
+  $('#file-picker-form').hide()
+  $('#get-uploads-link').hide()
+  $('#get-user-uploads-link').hide()
+  $('#sign-in-link').show()
+  $('#sign-up-link').show()
+  $('#sign-in-message').text('Please Sign In to Upload files.')
+  $('#sign-in-form').modal('show')
+  $('#uploads-thumbnails').hide()
+  $('#no-uploads-message').hide()
+  $('#file-upload-all-link').hide()
+  store.user = null
+}
+
+const signOutSuccess = () => {
+  initializeForm()
+  alertCallerAuthSuccess('frontSuccess', 'Sign-Out Success')
+}
+
+const signOutFailure = () => {
+  alertCallerAuthFailure('frontError', 'Sign-Out Failure')
+}
+
+module.exports = {
+  initializeForm,
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
